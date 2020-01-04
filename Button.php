904e2bd9787022fa9267b35c5255b4acf115a67a
VaishuSivan <41150319+VaishuSivan@users.noreply.github.com>
2020-01-04 13:27:51 +0530

    Initial commit

---
+++ Button.php
@@ -0,0 +1,29 @@
+<html> 
+<head> 
+<title>Button page</title> 
+</head> 
+<body> 
+<style> 
+body{ 
+background-image:lineargradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)),url("back10.jpg"); 
+height: 100vh;  
+/* Center and scale the image nicely */ 
+    background-position: center; 
+    background-repeat: no-repeat; 
+    background-size: cover; 
+} 
+</style> 
+<nav> 
+<ul> 
+<li><a href="signin.php"  style="margin-left: 550px; color: #80ff80; font-family: 'Lucida Calligraphy', sans-serif;font-size: 18px;">Signin</a></li></ul> 
+<ul> 
+<li><a href="message.php" style="margin-left: 550px; color: #80ff80; font-family: 'Lucida Calligraphy', sans-serif;font-size: 18px;">Message</a></li></ul> 
+<ul> 
+<li><a href="inbox.php" style="margin-left: 550px; color: #80ff80; font-family: 'Lucida Calligraphy', sans-serif;font-size: 18px;">Check Inbox</a> 
+</li></ul> 
+<ul> 
+<li><a href="signout.php" style="margin-left: 550px; color: #80ff80; font-family: 'Lucida Calligraphy', sans-serif;font-size: 18px;">Sign Out</a> 
+</li></ul> 
+</nav> 
+</body> 
+</html> 
